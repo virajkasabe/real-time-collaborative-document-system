b22f3873611e444fe535371bcc3621fc23e0688a
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-07-13 08:25:17 +0530

    remove user,collab,doc dir on test dir and add the files on that

---
+++ backend/test/user.test.js
@@ -0,0 +1,23 @@
+
+
+// ?? -- register
+import { otpGenerator } from "../src/utils/helper";
+
+describe("OtpTester", ()=>{
+   
+    it("should create a user ",  async() => {
+        const otp = otpGenerator()
+        console.log("otp", otp)
+    });
+
+}); 
+
+
+// ?? -- login
+
+
+// ?? -- get me
+
+
+// ?? -- logout
+
