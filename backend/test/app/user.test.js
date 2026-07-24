35e51395fe131c3b6680a20ced30e6437f2949b3
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-07-24 11:50:20 +0530

    add test case of regiter in user.test.js

--- backend/test/app/user.test.js
+++ backend/test/app/user.test.js
@@ -1,12 +1,36 @@
 import { getApiContext } from './comman'
+import { clearDB } from '../db';
 
 describe('User', () => { 
     beforeAll(async ({ playwright }) => {
         apiContext = await getApiContext(playwright);
-        // TODO : You can add other test cases like
-        // ?? 1. ClearDB()
-        // ?? 2. apiContext.dispose()
+        await clearDB()
     });
+
+    beforeAll(async({})=>{
+        await apiContext.dispose()
+    })
+
+    test.describe("POST:/api/v1/rtcds/auth -FOR USERS",() => {
+        // user register 
+        test("should create User with valid fullName, email & password", async()=>{
+            const user = {
+                fullName : "Greate Example",
+                email : "greateexample@gmail.com",
+                password : "Greate@123"
+            }
+
+            const res = await apiContext.post('/api/v1/rtcds/auth/register',{
+                data : user
+            })
+
+            const json = await res.json();
+            expect(res.status()).toEqual(201);
+            expect(json.statusCode).toEqual(201);
+            expect(json.data).toMatchObject(user);
+            userId = json.data._id;
+        })
+    })
 
         
 
